c79d0e110a99f9f1ddc9bbf1456bad481c33a440
paranoider <jerryqiyuan@hotmail.com>
2017-10-20 21:12:15 +0000

    Add different routes. Use of parameters in routes. Use of url_for.

--- hello.py
+++ hello.py
@@ -1,13 +1,30 @@
 import os # only needed in cloud 9
 
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/username/<username>')
+def show_user_profile(username):
+    return 'User ' + str(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post ' + str(post_id)
+
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
+    
+@app.route('/show_url')
+def show_url():
+    return url_for('show_user_profile', username='Ouyang')
     
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0') # only needed in cloud 9
     port = int(os.getenv('PORT', 5000)) # only needed in cloud 9
+    app.debug = True
     app.run(host=host, port=port)

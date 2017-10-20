1ade55446e48dc846a5d9780ee337e85ba7de2eb
paranoider <jerryqiyuan@hotmail.com>
2017-10-20 22:41:01 +0000

    Addjust the order of routes chronologically. Add template (HTML).

--- hello.py
+++ hello.py
@@ -1,11 +1,27 @@
 import os # only needed in cloud 9
 
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Index Page'
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/username/<username>')
+def show_user_profile(username):
+    return 'User ' + str(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post ' + str(post_id)
+    
+@app.route('/show_url')
+def show_url():
+    return url_for('show_user_profile', username='Ouyang')
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -20,22 +36,11 @@ def login2():
         return 'username is ' + request.values["username"]
     else:
         return '<form method="post" action="/login2"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
-
-@app.route('/username/<username>')
-def show_user_profile(username):
-    return 'User ' + str(username)
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Post ' + str(post_id)
-
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
     
-@app.route('/show_url')
-def show_url():
-    return url_for('show_user_profile', username='Ouyang')
+@app.route('/hello2')
+@app.route('/hello2/<name>')
+def hello2(name=None):
+    return render_template('hello.html', name=name)
     
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0') # only needed in cloud 9
